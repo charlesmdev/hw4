72d177b65d857e05487b8267bb15164b8bfaf9b6
CharlesM <charlesvincent.manaois@sjsu.edu>
2023-10-09 20:06:10 -0700

    Added TypeError exception for merge.py

--- merge.py
+++ merge.py
@@ -4,10 +4,18 @@ def merge_list(list1, list2):
     return sort(list1)
 #Selection Sort
 def sort(unsorted):
-    for i in range(len(unsorted)):
-        min_value = i;
-        for j in range(i + 1, len(unsorted)):
-            if unsorted[j] <= unsorted[min_value]:
-                min_value = j;
-        unsorted[i], unsorted[min_value] = unsorted[min_value], unsorted[i] 
-    return unsorted
+    try:
+        for i in range(len(unsorted)):
+            if not isinstance(i, int):
+                raise TypeError
+            min_value = i;
+            for j in range(i + 1, len(unsorted)):
+                if unsorted[j] <= unsorted[min_value]:
+                    min_value = j;
+            unsorted[i], unsorted[min_value] = unsorted[min_value], unsorted[i] 
+        return unsorted
+    except TypeError as e:
+        return "TypeError"
+
+    
+
