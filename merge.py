9bb5f59b08068a984b00e4617ce78cc22b938385
CharlesM <charlesvincent.manaois@sjsu.edu>
2023-10-09 19:55:17 -0700

    Added merge_list function and sort function

---
+++ merge.py
@@ -0,0 +1,13 @@
+def merge_list(list1, list2):
+    for x in list2:
+        list1.append(x)
+    return sort(list1)
+#Selection Sort
+def sort(unsorted):
+    for i in range(len(unsorted)):
+        min_value = i;
+        for j in range(i + 1, len(unsorted)):
+            if unsorted[j] <= unsorted[min_value]:
+                min_value = j;
+        unsorted[i], unsorted[min_value] = unsorted[min_value], unsorted[i] 
+    return unsorted
