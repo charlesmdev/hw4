e973801ceafb9e6934cbe7bfa99c17bd0654c095
CharlesM <charlesvincent.manaois@sjsu.edu>
2023-10-11 17:28:40 -0700

    Attempt to fix error handling

--- ladder.py
+++ ladder.py
@@ -4,14 +4,9 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 def my_steps(n):
-    try:
         if 1 <= n <= 25:
             return fib(n + 1)
         else:
-            raise Exception 
-    except Exception as e:
-        return "ValueError"
-       
-
-
-
+            raise ValueError("ValueError")
+        
+        
--- merge.py
+++ merge.py
@@ -4,18 +4,16 @@ def merge_list(list1, list2):
     return sort(list1)
 #Selection Sort
 def sort(unsorted):
-    try:
         for i in range(len(unsorted)):
             if not isinstance(i, int):
-                raise TypeError
+                raise TypeError("TypeError")
             min_value = i;
             for j in range(i + 1, len(unsorted)):
                 if unsorted[j] <= unsorted[min_value]:
                     min_value = j;
             unsorted[i], unsorted[min_value] = unsorted[min_value], unsorted[i] 
         return unsorted
-    except TypeError as e:
-        return "TypeError"
+
 
     
 
