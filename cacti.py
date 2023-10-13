4ca8ddd856a1b084753acbffdf1a311905f040cb
CharlesM <charlesvincent.manaois@sjsu.edu>
2023-10-12 19:57:00 -0700

    First implementation of cacti.py

--- cacti.py
+++ cacti.py
@@ -1,2 +1,20 @@
+def cacti_number(map):
+    count = 0
+    row = len(map)
+    column = len(map[0])
+    def is_valid(x, y):
+            return 0 <= x < row and 0 <= y < column and map[x][y] == 0
+    for x in range(row):
+            for y in range(col):
+                if map[x][y] == 0:
+                    if (
+                        is_valid(x - 1, y) or is_valid(x + 1, y) or
+                        is_valid(x, y - 1) or is_valid(x, y + 1)
+                    ):
+                        count += 1
+    return count
+
+
+
 
 
